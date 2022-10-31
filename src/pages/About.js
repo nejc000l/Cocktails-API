b033001cde6d2ecd9d3dc214a59eb438d5ca1c18
nejc000l <nejc.lampic@gmail.com>
2022-10-31 18:07:31 +0100

    cm

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum facere eligendi doloribus tempora, laudantium sunt, illo voluptate vitae non incidunt quisquam inventore nam, officia provident. Sunt saepe porro reiciendis beatae!
+      </p>
+    </section>
+  )
+}
+
+export default About
